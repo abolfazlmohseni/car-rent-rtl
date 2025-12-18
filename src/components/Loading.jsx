6f734997327864eacb1bb88b28a0722b1fd72a08
abolfazlmohseni <abolfazlmohseni605@gmail.com>
2025-12-18 09:57:24 +0330

    add component loding and navbar

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="absolute text-gray-600 text-xl h-screen w-screen text-center content-center bg-gray-400/20">
+            <p className=" animate-bounce">در حال بارگذاری ...</p>
+        </div>
+    );
+}
+
+export default Loading;
